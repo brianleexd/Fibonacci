abb543793793f4f88f9b8de2d05e429a077161ba
Brian Lee <brianleeof1994@gmail.com>
2017-05-24 17:25:14 -0400

    sanitized input, still need to fix negative integers print out statement added

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -17,13 +17,16 @@ public class Fibonacci {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       int value = -1;
-       while(value < 0){
+       int value = 1;
        Scanner in = new Scanner(System.in);
-       System.out.println("Enter the abmount of fibonacci numbers you want to see:");
+       System.out.println("Enter the amount of Fibonacci numbers you want to see");
+       while (!in.hasNextInt()){
+           System.out.println("That is an invalid input");
+           System.out.println("Enter the amount of Fibonacci numbers you want to see");
+           in.next();
+       }       
        value = in.nextInt();
-       }
-       fibonacci(value);
+       printFibonacci(fibonacci(value));
     }
     
    public static int[] fibonacci(int value){
@@ -32,8 +35,12 @@ public class Fibonacci {
        fibnumbers[1] = 1;
        for (int n = 2; n < value; n++){
            fibnumbers[n] = fibnumbers [n-1] + fibnumbers[n-2];
-           System.out.println(fibnumbers[n]);
        }
        return fibnumbers;
    }
+   public static void printFibonacci(int[] fibs){
+       for (int n = 0; n < fibs.length; n++){
+       System.out.println(n + 1 + ": " + fibs[n]);
+   }
+}
 }
