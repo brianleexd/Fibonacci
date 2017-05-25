bcfba6f12f5e99fa4bf4f35eccfc387b9392ecc8
Brian Lee <brianleeof1994@gmail.com>
2017-05-24 20:09:23 -0400

    works for all inputs, probably a way to merge input sanitation check but not sure how

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -17,20 +17,26 @@ public class Fibonacci {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       int value = 1;
+       int value = 0;
        Scanner in = new Scanner(System.in);
-       System.out.println("Enter the amount of Fibonacci numbers you want to see");
-       while (!in.hasNextInt()){
-           System.out.println("That is an invalid input");
-           System.out.println("Enter the amount of Fibonacci numbers you want to see");
-           in.next();
-       }       
-       value = in.nextInt();
+       System.out.println("Enter the amount of Fibonacci numbers to display");
+       while (value <= 0){
+            while (!in.hasNextInt()){
+                System.out.println("That is an invalid input");
+                System.out.println("Enter the amount of Fibonacci numbers to display");
+                in.next();
+       }
+            value = in.nextInt();
+            if (value <= 0){
+                System.out.println("That is an invalid input");
+                System.out.println("Enter the amount of Fibonacci numbers to display");
+       }
+       }
        printFibonacci(fibonacci(value));
     }
     
    public static int[] fibonacci(int value){
-       int[] fibnumbers = new int[value];
+       int[] fibnumbers = new int[value + 1];
        fibnumbers[0] = 1;
        fibnumbers[1] = 1;
        for (int n = 2; n < value; n++){
@@ -39,7 +45,7 @@ public class Fibonacci {
        return fibnumbers;
    }
    public static void printFibonacci(int[] fibs){
-       for (int n = 0; n < fibs.length; n++){
+       for (int n = 0; n < fibs.length - 1; n++){
        System.out.println(n + 1 + ": " + fibs[n]);
    }
 }
